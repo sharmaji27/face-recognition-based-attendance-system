b698315c3891b1c3ea1c2c0c6d7d810467bf0ac7
Abhishek Sharma <asharma70420@gmail.com>
2023-03-08 13:01:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,13 +18,18 @@ datetoday2 = date.today().strftime("%d-%B-%Y")
 
 
 #### Initializing VideoCapture object to access WebCam
-face_detector = cv2.CascadeClassifier('static/haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture(0)
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+try:
+    cap = cv2.VideoCapture(1)
+except:
+    cap = cv2.VideoCapture(0)
 
 
 #### If these directories don't exist, create them
 if not os.path.isdir('Attendance'):
     os.makedirs('Attendance')
+if not os.path.isdir('static'):
+    os.makedirs('static')
 if not os.path.isdir('static/faces'):
     os.makedirs('static/faces')
 if f'Attendance-{datetoday}.csv' not in os.listdir('Attendance'):
@@ -39,10 +44,12 @@ def totalreg():
 
 #### extract the face from an image
 def extract_faces(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    face_points = face_detector.detectMultiScale(gray, 1.3, 5)
-    return face_points
-
+    if img!=[]:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_points = face_detector.detectMultiScale(gray, 1.3, 5)
+        return face_points
+    else:
+        return []
 
 #### Identify face using ML model
 def identify_face(facearray):
