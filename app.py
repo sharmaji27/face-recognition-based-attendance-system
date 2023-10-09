17d3bd10aad170ec3da6f4ac04368203d11eea60
sharmaji27 <asharma70420@gmail.com>
2023-10-09 17:12:33 +0530

    fixed app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ import joblib
 # Defining Flask App
 app = Flask(__name__)
 
+# Number of images to take for each user
 nimgs = 10
 
 # Saving Date today in 2 different formats
 datetoday = date.today().strftime("%m_%d_%y")
-datetoday2 = date.today().strftime("%d-%B-%Y")
 
 
 # Initializing VideoCapture object to access WebCam
@@ -110,15 +110,6 @@ def getallusers():
     return userlist, names, rolls, l
 
 
-## A function to delete a user folder 
-def deletefolder(duser):
-    pics = os.listdir(duser)
-    for i in pics:
-        os.remove(duser+'/'+i)
-    os.rmdir(duser)
-
-
-
 
 ################## ROUTING FUNCTIONS #########################
 
@@ -126,33 +117,7 @@ def deletefolder(duser):
 @app.route('/')
 def home():
     names, rolls, times, l = extract_attendance()
-    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2)
-
-
-## List users page
-@app.route('/listusers')
-def listusers():
-    userlist, names, rolls, l = getallusers()
-    return render_template('listusers.html', userlist=userlist, names=names, rolls=rolls, l=l, totalreg=totalreg(), datetoday2=datetoday2)
-
-
-## Delete functionality
-@app.route('/deleteuser', methods=['GET'])
-def deleteuser():
-    duser = request.args.get('user')
-    deletefolder('static/faces/'+duser)
-
-    ## if all the face are deleted, delete the trained file...
-    if os.listdir('static/faces/')==[]:
-        os.remove('static/face_recognition_model.pkl')
-    
-    try:
-        train_model()
-    except:
-        pass
-
-    userlist, names, rolls, l = getallusers()
-    return render_template('listusers.html', userlist=userlist, names=names, rolls=rolls, l=l, totalreg=totalreg(), datetoday2=datetoday2)
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg())
 
 
 # Our main Face Recognition functionality. 
@@ -160,7 +125,7 @@ def deleteuser():
 @app.route('/start', methods=['GET'])
 def start():
     if 'face_recognition_model.pkl' not in os.listdir('static'):
-        return render_template('home.html', totalreg=totalreg(), datetoday2=datetoday2, mess='There is no trained model in the static folder. Please add a new face to continue.')
+        return render_template('home.html', totalreg=totalreg(), mess='There is no trained model in the static folder. Please add a new face to continue.')
 
     ret = True
     cap = cv2.VideoCapture(0)
@@ -181,7 +146,7 @@ def start():
     cap.release()
     cv2.destroyAllWindows()
     names, rolls, times, l = extract_attendance()
-    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2)
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg())
 
 
 # A function to add a new user.
@@ -217,7 +182,7 @@ def add():
     print('Training Model')
     train_model()
     names, rolls, times, l = extract_attendance()
-    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2)
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg())
 
 
 # Our main function which runs the Flask App
